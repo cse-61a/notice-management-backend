c53f564be008ce7fe540d3726989a156af788fa5
cse-61a <cse.61.a@gmail.com>
2024-11-27 21:59:33 +0600

    cors headers added

--- cse_61a_backend/settings.py
+++ cse_61a_backend/settings.py
@@ -60,11 +60,15 @@ INSTALLED_APPS = [
 
     'student',
     'notice',
+    "corsheaders",
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
