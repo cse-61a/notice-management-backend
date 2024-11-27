4060086a6340c3f3713d2bf33f6dc435fae0e5b0
cse-61a <cse.61.a@gmail.com>
2024-11-28 01:21:45 +0600

    related name for course added

--- notice/serializers.py
+++ notice/serializers.py
@@ -7,10 +7,16 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class NoticeSerializer(serializers.ModelSerializer):
+    course_name = serializers.SerializerMethodField()  # Add a custom field for the course name
+
     class Meta:
         model = Notice
         fields = [
-            'id', 'description', 'notice_type', 'date',
-            'course', 'created_at'
+            'id', 'description', 'notice_type', 'resource_link', 'date',
+            'course', 'course_name', 'created_at'
         ]
         read_only_fields = ['created_at']
+
+    def get_course_name(self, obj):
+        # Retrieve the course name if a course is associated
+        return obj.course.name if obj.course else None
