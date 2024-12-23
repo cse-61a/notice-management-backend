aa9e0a7e11a25b7fb18279f83d5d1a7b33a7f0a2
cse-61a <cse.61.a@gmail.com>
2024-12-23 23:36:35 +0600

    Update settings.py

--- cse_61a_backend/settings.py
+++ cse_61a_backend/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
+    'https://notice-management-backend.onrender.com/',
     'https://*.127.0.0.1',
     'http://127.0.0.1:5500',
 ]
